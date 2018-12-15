640ec19662265f51d155051ff9547a9626a5c4cc
Rafael Marques <rafaelomarques@gmail.com>
2018-12-15 13:35:09 -0200

    removed status print

--- example-2.py
+++ example-2.py
@@ -15,9 +15,8 @@ class Blog:
 def get_content(url):
     try:
         r = requests.get(url, allow_redirects=True)
-        print(r.status_code)
         r.raise_for_status()
-        
+
         return BeautifulSoup(r.content, 'html.parser')
     except:
         return None
@@ -33,12 +32,12 @@ blog = Blog(bs.title.string, [])
 while bs is not None:
     content_box = bs.find('div', {'id': 'content_box'})
     articles = content_box.find_all('article', class_='latestPost')
-    
+
     for article in articles:
         title = article.find('span', class_='p-name')
         if not title:
             continue
-        
+
         url = article.find('a')['href']
         excerpt = article.find('div', class_='front-view-content')
         blog.add_post({
@@ -46,7 +45,7 @@ while bs is not None:
             'url': url,
             'excerpt': excerpt.get_text()
         })
-    
+
     current_page += 1
     print('going to page ' + str(current_page))
     bs = get_content(f'{base_url}/page/{current_page}')
